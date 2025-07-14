902414702d9aed7ab60ea3c792ca99a5bc9b0a11
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-07-14 18:34:44 -0300

    Fix:Bugs

--- src/containers/Detail/styles.js
+++ src/containers/Detail/styles.js
@@ -95,3 +95,35 @@ p {
 
 
 `
+
+
+export const ContainerMovies = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding: 20px;
+width: 100%;
+
+div {
+    display: flex;
+    flex-direction: column;
+    max-width: 1000px;
+    width: 100%;
+    height: 100%;
+    margin: 20px 0;
+}
+
+h4 {
+color: #ffffff;
+font-size: 20px;
+font-weight: 700;
+margin-bottom: 10px;
+}
+
+iframe {
+border: none;
+}
+
+`
+
